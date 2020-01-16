ecef1bcc27ebe9a77b888e77d7e76b740ee12868
anishLushte07 <lushteanish@gamil.com>
2020-01-16 12:57:48 +0530

    added expiry check functionality

--- models/ShortURL.js
+++ models/ShortURL.js
@@ -7,16 +7,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const options = {
-  versionKey : false
+  versionKey : false,
 };
 
-module.exports = function (opt = {}) {
+module.exports = function () {
   const ShortURLSchema = new Schema({
-    _id        : { type : String, unique: true },
-    URL        : { type : String, unique: false },
+    _id        : { type : String },
+    URL        : { type : String },
     hits       : { type : Number, default: 0 },
     created_at : { type : Date, default: Date.now },
-    expires_at : { type : Date, default: Date.now },
+    expires_at : { type : Date, default: null },
   }, options);
 
   return mongoose.model('ShortURL', ShortURLSchema);
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const URLShortner = require('./lib/index');
 
-const instance = new URLShortner('mongodb://192.168.0.161/shortdb', (err) => console.log(err));
+const instance = new URLShortner('mongodb://192.168.0.161/shortdb', (err) => console.log(err), { domain: 'https:click.com' });
 // short.connect('mongodb://192.168.0.161/shortdb');
 
 /*instance.connection.on('error', (error) => {
@@ -8,7 +8,14 @@ const instance = new URLShortner('mongodb://192.168.0.161/shortdb', (err) => con
 });*/
 
 setTimeout(() => {
-  instance.shortenUrl('https://asdfasdf.sadf.asdf/asdf/sadf')
+  instance.shortenUrl('https://asdfsadf.asdf.sadf/sadf', new Date())
     .then(res => console.log('res' , res))
     .catch(err => console.log(err));
+}, 2000);
+
+
+setTimeout(() => {
+  instance.retrieve('0004', new Date())
+      .then(res => console.log('res' , res))
+      .catch(err => console.log(err));
 }, 2000);
